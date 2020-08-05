31b07cca54eb338d8a27234a067dea3282bd677f
Felixatika <felixatika@gmail.com>
2020-08-05 21:20:47 +0300

    updload test1 files

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $("h1").click(function () {
+    alert("This is header 1");
+  });
+
+  $("p").click(function () {
+    alert("This is a paragraph");
+  });
+  $("img").click(function () {
+    alert("This is an image!cute");
+  });
+});
